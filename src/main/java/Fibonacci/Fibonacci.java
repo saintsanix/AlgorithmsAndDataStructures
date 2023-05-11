02ce28c5375d5585bd2072e21880f1e8b981b176
aleksandr_shchennikov_1 <sshennikov@gmail.com>
2023-05-11 15:30:02 +0300

    init and create Naive, Effective methods

---
+++ src/main/java/Fibonacci/Fibonacci.java
@@ -0,0 +1,26 @@
+package Fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibEffective(1000));
+    }
+
+    private static long fibNaive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibNaive(n - 1) + fibNaive(n - 2);
+    }
+
+    private static long fibEffective(int n) {
+        long[] arr = new long[n + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
