5cc678de6c263d3e96f5766c33403ff3bc6ef59f
aleksandr_shchennikov_1 <sshennikov@gmail.com>
2023-05-11 15:44:46 +0300

    update fibNaive method memoization

--- src/main/java/Fibonacci/Fibonacci.java
+++ src/main/java/Fibonacci/Fibonacci.java
@@ -1,15 +1,29 @@
 package Fibonacci;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibEffective(1000));
+
+        int n = 10;
+        long[] mem = new long[n + 1];
+
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibNaive(n, mem));
     }
 
-    private static long fibNaive(int n) {
+    private static long fibNaive(int n, long[] mem) {
+
+        if (mem[n] != -1) {
+            return mem[n];
+        }
         if (n <= 1) {
             return n;
         }
-        return fibNaive(n - 1) + fibNaive(n - 2);
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+        mem[n] = result;
+        return result;
     }
 
     private static long fibEffective(int n) {
